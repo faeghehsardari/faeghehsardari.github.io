6f5bfb58dd11cd44f007c288a71748d9914791a3
faeghehsardari <faegheh.sardari@gmail.com>
2025-03-20 18:14:13 +0000

    Deploying to gh-pages from @ faeghehsardari/faeghehsardari.github.io@cce1f3faf13e9a58da49b80aadee7d6929bf28b0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -438,7 +438,7 @@ ninja.data = [{
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile-we-secured-a-50k-grant-50-funded-by-epsrc-and-50-by-tesco-to-advance-video-understanding-for-retail",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile: We secured a £50K...',
+          title: '% A simple inline announcement with Markdown emoji! :sparkles: :smile: We secured a...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
