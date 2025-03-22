7f749443186de24f738aea0f6599734b851b3126
faeghehsardari <faegheh.sardari@gmail.com>
2025-03-22 20:52:49 +0000

    Deploying to gh-pages from @ faeghehsardari/faeghehsardari.github.io@0895e23ac137b98488a5afe54a3c4d3f62450c85 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -40,35 +40,28 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%66.%73%61%72%64%61%72%69@%73%75%72%72%65%79.%61%63.%75%6B", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://github.com/faeghehsardari", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("/feed.xml", "_blank");
+          window.open("https://www.linkedin.com/in/faegheh-sardari-492b9154", "_blank");
         },
       },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=_8dV3CgAAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
