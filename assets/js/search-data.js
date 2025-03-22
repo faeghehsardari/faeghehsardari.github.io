cd626f2da2486f5ee95d3b50b08c1356b967f426
faeghehsardari <faegheh.sardari@gmail.com>
2025-03-22 21:23:39 +0000

    Deploying to gh-pages from @ faeghehsardari/faeghehsardari.github.io@57cf581718ab5d5aa5e349fcd79c913d7cbf8833 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,13 +16,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-teaching",
-          title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/teaching/";
-          },
         },{id: "news-our-paper-coleaf-is-accepted-at-eccv-2024",
           title: 'Our paper `CoLeaF’ is accepted at ECCV-2024',
           description: "",
