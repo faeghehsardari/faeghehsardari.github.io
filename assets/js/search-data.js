ce82424bba2f455cededc10080ff229c62bce9aa
faeghehsardari <faegheh.sardari@gmail.com>
2025-03-23 02:48:39 +0000

    Deploying to gh-pages from @ faeghehsardari/faeghehsardari.github.io@88af741b27900f3ece1316d8698d1010c968db6d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,13 +16,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-activities",
-          title: "activities",
-          description: "members of the lab or group",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/people/";
-          },
         },{id: "nav-people",
           title: "people",
           description: "members of the lab or group",
